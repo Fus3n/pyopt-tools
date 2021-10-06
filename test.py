f0f547dd407c3f0363844f410d8b879b58f2f332
FUSEN <flaminggaming56@gmail.com>
2021-10-06 21:50:49 +0600

    Updated string_tools

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-from pyopt_tools import string_tools
-from pyopt_tools import int_tools
+from pyopt_tools import string_tools, int_tools
 
 print(string_tools.reverse_string("dda"))
 print(int_tools.reverse_int(1234))
